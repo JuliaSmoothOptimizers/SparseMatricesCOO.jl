269b5d7b5dc09c95936c82adcd2a10d42e7b2ca0
dpo <dpo@users.noreply.github.com>
2021-10-23 03:41:57 +0000

    :robot: Format .jl files

--- src/coo_utils.jl
+++ src/coo_utils.jl
@@ -1,11 +1,14 @@
 struct DimensionError <: Exception
-  name :: Union{Symbol,String}
-  dim_expected :: Int
-  dim_found :: Int
+  name::Union{Symbol, String}
+  dim_expected::Int
+  dim_found::Int
 end
 
 function Base.showerror(io::IO, e::DimensionError)
-  print(io, "DimensionError: Input $(e.name) should have length $(e.dim_expected) not $(e.dim_found)")
+  print(
+    io,
+    "DimensionError: Input $(e.name) should have length $(e.dim_expected) not $(e.dim_found)",
+  )
 end
 
 # Check that arrays have a prescribed size.
@@ -14,10 +17,11 @@ macro lencheck(l, vars...)
   exprs = Expr[]
   for var in vars
     varname = string(var)
-    push!(exprs,
-          :(if length($(esc(var))) != $(esc(l))
-                throw(DimensionError($varname, $(esc(l)), length($(esc(var)))))
-            end))
+    push!(exprs, :(
+      if length($(esc(var))) != $(esc(l))
+        throw(DimensionError($varname, $(esc(l)), length($(esc(var)))))
+      end
+    ))
   end
   Expr(:block, exprs...)
 end
